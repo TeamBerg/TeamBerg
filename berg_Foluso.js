fe6eaef255c70fbdf3797be60e1e153e7755911e
Foluso Ogunfile <folusoogunfile@yahoo.com>
2021-08-05 15:53:47 +0100

    Print name, email, slack username, biostack

---
+++ berg_Foluso.js
@@ -0,0 +1,10 @@
+console.log("name", "Foluso Ogunfile");
+console.log("email", "fogunfile@gmail.com");
+console.log("slack", "@fogunfile");
+console.log("biostack", "Software Development");
+
+const printDetails = () => {
+    return "Name: Foluso Ogunfile Email: fogunfile@gmail.com Slack: @fogunfile Biostack: Software Development";
+}
+printDetails();
+
