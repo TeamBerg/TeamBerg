da67087cb6d73de3221809c6617011d26bebb57d
Abdulkabir Tola Ojulari <abdul@devlift.io>
2021-08-06 10:17:22 -0400

    Abdulkabir's Task0 updated file

--- task0-abdul.R
+++ task0-abdul.R
@@ -5,7 +5,7 @@ email <- 'abdulkabirojulari@gmail.com'
 slack <- '@abdulojus'
 twitter <- '@ojularias'
 biostack <- 'Gnomics'
-final <- list(Name= name, Email= email,Slack= slack, Twitter= twitter, Biostack= biostack)
 hamming_distance <- stringdist(slack, twitter, method="hamming")
 cat("Hamming Distance: ", hamming_distance )
+final <- list(Name= name, Email= email,Slack= slack, Twitter= twitter, Biostack= biostack, HammingDistance= hamming_distance)
 write.csv(final)
