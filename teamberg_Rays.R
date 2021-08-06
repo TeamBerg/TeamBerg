c037e374492187f0d12361fcd9750633c5ea809a
Ridwan Shittu <shittuar@gmail.com>
2021-08-06 15:17:37 +0200

    @rayscell change typo in code from Stack_username to Slack username

--- teamberg_Rays.R
+++ teamberg_Rays.R
@@ -2,11 +2,11 @@
 #1. Create strings variable to store the details without Hamming distance
 name="Ridwan Shittu"
 email="shittuar@gmail.com"
-stack_username="@Rays"
+slack_username="@Rays"
 biostack= "Data analytics" 
 twitter_handle ="@ridwanshittu30"
 #bring all together
-details_rays <- c(name, email, stack_username, biostack, twitter_handle)
+details_rays <- c(name, email, slack_username, biostack, twitter_handle)
 print(details_rays)
 
 
@@ -19,5 +19,5 @@ Hamming_distance <- function(a,b){
 }
 
 #3. Print out the details for username:rays with Hamming distance calculated for the slack_username and twitter_handle
-details_rays <- c(name,email, stack_username, biostack, twitter_handle, Hamming_distance(stack_username, twitter_handle))
+details_rays <- c(name,email, slack_username, biostack, twitter_handle, Hamming_distance(slack_username, twitter_handle))
 print(details_rays)
