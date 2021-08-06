9822975b711ca5e8838a49912eae97d55bb70d03
Nirvana-Munir <69080911+Nirvana-Munir@users.noreply.github.com>
2021-08-06 22:16:50 +0200

    Update teamberg_Nirvana.py

--- teamberg_Nirvana.py
+++ teamberg_Nirvana.py
@@ -16,5 +16,7 @@ def hamming_distance(slack_username, twitter_handle):
 slack_username = "@Nirvana"
 twitter_handle = "@Nervana"
 
+hamming_distance = hamming_distance(slack_username, twitter_handle)
+
 print ("Name: "+name, "Email: "+email, "Slack Username: "+slack_username, "Biostack: "+biostack, "twitter_handle: "+twitter_handle, "hamming Distance: "+str(hamming_distance), sep = "\n")
 
