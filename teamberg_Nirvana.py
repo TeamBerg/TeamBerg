8d5a8bfd938f6d24fe796d9852ba40590027ec07
Nirvana-Munir <69080911+Nirvana-Munir@users.noreply.github.com>
2021-08-07 00:40:03 +0200

    Update teamberg_Nirvana.py

--- teamberg_Nirvana.py
+++ teamberg_Nirvana.py
@@ -18,5 +18,47 @@ twitter_handle = "@Nervana"
 
 hamming_distance = hamming_distance(slack_username, twitter_handle)
 
-print ("Name: "+name, "Email: "+email, "Slack Username: "+slack_username, "Biostack: "+biostack, "twitter_handle: "+twitter_handle, "hamming Distance: "+str(hamming_distance), sep = "\n")
+name = "Nirvana Munir"
+email = "nirvana.monir66@gmail.com"
+slack_username = "@Nirvana"
+biostack =  "Drug Development"
+twitter_handle = "@Nervana"
+
+
+def hamming_distance(slack_username, twitter_handle):
+    count = 0
+    i = 0
+    while i < len(slack_username):
+        if(slack_username[i] != twitter_handle[i]):
+            count += 1
+        i += 1
+    return count         
+slack_username = "@Nirvana"
+twitter_handle = "@Nervana"
+
+hamming_distance = hamming_distance(slack_username, twitter_handle)
+
+name = "Nirvana Munir"
+email = "nirvana.monir66@gmail.com"
+slack_username = "@Nirvana"
+biostack =  "Drug Development"
+twitter_handle = "@Nervana"
+
+
+def hamming_distance(slack_username, twitter_handle):
+    count = 0
+    i = 0
+    while i < len(slack_username):
+        if(slack_username[i] != twitter_handle[i]):
+            count += 1
+        i += 1
+    return count         
+slack_username = "@Nirvana"
+twitter_handle = "@Nervana"
+
+hamming_distance = hamming_distance(slack_username, twitter_handle)
+
+print ("Name: "+name, "Email: "+email, "Slack Username: "+slack_username, "Biostack: "+biostack, "twitter_handle: "+twitter_handle, "hamming Distance: "+str(hamming_distance))
+
+
 
