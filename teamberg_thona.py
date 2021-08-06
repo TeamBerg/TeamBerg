383768f330dca540a47b18a43e8161a71831cff4
Wusu Thona <thonawusu@gmail.com>
2021-08-06 18:57:21 +0100

    @debby task done with python

---
+++ teamberg_thona.py
@@ -0,0 +1,28 @@
+
+def task_result(name, email, slack_username, biostack, twitter_handle, hamming_distance):
+    print("name: "+ name )
+    print("email: "+ email)
+    print("slack_username: "+ slack_username )
+    print("biostack: "+ biostack )
+    print("twitter_handle: "+ twitter_handle )
+    print("hamming_distance: "+ hamming_distance)
+
+
+
+def hamming_distance(slack_username, twitter_username):
+    counter = 0
+    for y in range(len(slack_username)):
+        if slack_username[y] !=twitter_username[y]:
+            counter+=1
+    return str(counter)
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+  task_result("wusu deborah thona","thonawusu@gmail.com","@debby","drug development","@thonawusu",
+  hamming_distance ("@debby","@thonawusu"))
